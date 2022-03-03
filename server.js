fb7cb6e61801d5ac3f5cd4f87722f54689bd6365
Alex Diebolt <alexander.diebolt@gmail.com>
2022-03-03 12:00:02 -0800

    setting up server routes

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+const noteApi = require('./routes/noteRoutes');
+const indexApi = require('./routes/indexRoutes.js');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Import custom middleware, "cLog"
-app.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use('/api', noteApi);
+app.use('/', indexApi)
 
 app.use(express.static('public'));
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`))
