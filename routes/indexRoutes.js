5d17e25384eaabf40d5278cc6e6894d4962f80f5
Alex Diebolt <alexander.diebolt@gmail.com>
2022-03-10 08:35:14 -0800

    adding routes

---
+++ routes/indexRoutes.js
@@ -0,0 +1,11 @@
+const index = require('express').Router()
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+
+  router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+
+module.exports = index
