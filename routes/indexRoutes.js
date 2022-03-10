0486b6b260f9e9c9ec8a474ccf5f20bc4b0be61d
Alex Diebolt <alexander.diebolt@gmail.com>
2022-03-10 09:49:22 -0800

    working on routes

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,11 +1,12 @@
-const index = require('express').Router()
+const router = require('express').Router()
+const path = require('path')
 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
 
-  router.get('*', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
-module.exports = index
+module.exports = router
--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,2 +1,48 @@
 const notes = require('express').Router()
+const fs = require('fs')
+const util = require('util')
+const { v4: uuidv4 } = require('uuid')
+
+const readFromFile = util.promisify(fs.readFile)
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
+
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
+
+notes.get('/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/notes', (req, res) => {
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully 🚀`);
+    } else {
+        res.error('Error in adding Note');
+    }
+});
+
+module.exports = notes
 
